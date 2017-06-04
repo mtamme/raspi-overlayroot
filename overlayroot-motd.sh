18cd87b853a4cd37a54074f6a956c1650034dbf6
mtamme <mtamme@users.noreply.github.com>
2017-06-04 20:16:56 +0200

    Print warning in color.

--- overlayroot-motd.sh
+++ overlayroot-motd.sh
@@ -1,8 +1,8 @@
 if grep overlayroot /proc/cmdline > /dev/null; then
 
-echo -e "\e[1;31m"
-echo -e "Warning: Changes to the root filesystem will not be preserved after reboot!"
-echo -e "Remove the 'overlayroot' argument in '/boot/cmdline.txt' to preserve changes."
-echo -e "\e[0m"
+echo -e "\e[1;31m
+Warning: Changes to the root filesystem will not be preserved after reboot!
+Remove the 'overlayroot' argument in '/boot/cmdline.txt' to preserve changes.
+\e[0m"
 
 fi
